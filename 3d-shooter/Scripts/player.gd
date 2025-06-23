e43084aa1105e720773489b7e085f818ec7a7793
Thomas0938 <tgriffin@students.mags.school.nz>
2025-06-23 13:27:36 +1200

    finished dashing

--- 3d-shooter/Scripts/player.gd
+++ 3d-shooter/Scripts/player.gd
@@ -7,3 +7,40 @@ var double_jump: float = true
 var double_jump_velocity: int = 6.0
 @onready var pivot: Node3D = $Node3D
 @export var sens: int = 0.1
+var jumping: float = false
+var dash_direction
+var is_dashing: float = false
+var dash_velocity: int = 10
+var dash_duration: int = 0.1
+var time: int = 0
+var has_dashed: float = false
+var gravity: int = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _physics_process(delta: float) -> void:
+	# The gravity
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_velocity
+		jumping = true
+		double_jump = true
+	elif Input.is_action_just_pressed("ui_accept") and double_jump:
+		velocity.y = double_jump_velocity
+		double_jump = false
+
+	var input_direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction := (transform.basis * Vector3(input_direction.x, 0, input_direction.y)).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+		
+	if is_dashing:
+		time += delta
+		velocity += dash_direction * -dash_velocity
+		velocity.y = 0
+	
+	if is_on_floor() and has_dashed:
+		has_dashed = false
