87fcd2e7e8f045343922e30767e20c6378f9e052
Thomas0938 <tgriffin@students.mags.school.nz>
2025-06-25 09:48:04 +1200

    creating a cooldown and fixing the camera bug

--- 3d-shooter/Scripts/player.gd
+++ 3d-shooter/Scripts/player.gd
@@ -1,19 +1,21 @@
 extends CharacterBody3D
 class_name Player
-
-var speed: int = 8.0
-var jump_velocity: int = 6.0
-var double_jump: float = true
-var double_jump_velocity: int = 6.0
+# a float has a decemal place where int does not
+var speed: int = 8
+var jump_velocity: int = 6
+var double_jump: bool = true
+var double_jump_velocity: int = 6
 @onready var pivot: Node3D = $Node3D
-@export var sens: int = 0.5
-var jumping: float = false
+@export var sens: float = 0.1
+var jumping: bool = false
 var dash_direction
-var is_dashing: float = false
+var is_dashing: bool = false
 var dash_velocity: int = 10
-var dash_duration: int = 0.1
+var dash_duration: float = 0.1
+var dash_cooldown: int = 30
+var Dash_cooldown: bool = true
 var time: int = 0
-var has_dashed: float = false
+var has_dashed: bool = false
 var gravity: int = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _physics_process(delta: float) -> void:
@@ -47,8 +49,14 @@ func _physics_process(delta: float) -> void:
 
 	if Input.is_action_just_pressed("dash"):
 		is_dashing = true
+		$Timer.start(dash_duration)
 		dash_direction = transform.basis.z
 		has_dashed = true
+
+#This gives the dash a cooldown
+	if Input.is_action_just_pressed("dash") and Dash_cooldown:
+		Dash_cooldown = false
+		$Timer2.start()
 
 	move_and_slide()
 
@@ -56,7 +64,21 @@ func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(event) -> void:
+#	for the camera movement. This wasn't working because I was using int when i should have used a float.
 	if event is InputEventMouseMotion:
+		print("MOUSE")
 		rotate_y(deg_to_rad(-event.relative.x * sens))
 		pivot.rotate_x(deg_to_rad(-event.relative.y * sens))
 		pivot.rotation.x = clamp(pivot.rotation.x, deg_to_rad(-50), deg_to_rad(40))
+
+func _on_timer_timeout() -> void:
+	is_dashing = false
+	velocity.y = 0
+
+#To allow you to dash again after cooldown
+func _dash_cooldown() -> void:
+	Dash_cooldown = true
+	#$Timer2.start(dash_cooldown)
+	#Dash_cooldown = false
+	#has_dashed = true
+	#is_dashing = false
