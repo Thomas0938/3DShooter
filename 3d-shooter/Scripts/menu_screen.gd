4ba79514fcf249dadcdf34fec5b39ca8f6c2da02
Thomas0938 <tgriffin@students.mags.school.nz>
2025-10-16 18:06:40 +1300

    added tutorial button

--- 3d-shooter/Scripts/menu_screen.gd
+++ 3d-shooter/Scripts/menu_screen.gd
@@ -17,3 +17,7 @@ func _on_play_button_pressed() -> void:
 # This will close the game when the exit is hit.
 func _on_exit_button_2_pressed() -> void:
 	get_tree().quit()
+
+
+func _tutorial() -> void:
+	get_tree().change_scene_to_file("res://Scenes/tutorial.tscn")
